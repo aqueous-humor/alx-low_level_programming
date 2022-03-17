394ac6716bf0bfb6091204d018d573745a2e982b
root <akpos97@ymail.com>
2022-03-16 21:57:37 -0700

    Program prints string without using standard library

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,17 +7,15 @@
 */
 int main(void)
 {
-char str[8] = "_putchar";
-  
-int i;
-  
-  for (i = 0; i < 9; i++)
-    
-  {
-   _putchar(str[i]);    
-  }
-  
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');  
+_putchar('c');  
+_putchar('h');
+_putchar('a');
+_putchar('r');  
 _putchar('\n');
-
+  
 return (0); 
 }
