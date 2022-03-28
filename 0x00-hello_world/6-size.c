c6736a57fb9b51f81696159125fbc5a4a9ebe967
aqueous-humor <akpos97@ymail.com>
2022-03-28 09:16:00 +0000

    Program prints some text along with the size in bytes of various data types in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - This function prints the sizes of various data types on the computer it is compiled and run on.
+ * void - no parameter value
+ * Return : 0
+*/
+int main (void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));printf("Size of a char: %d byte(s)\n", sizeof(char));
+return (0)
+}
